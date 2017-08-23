198d9a5519414a83aa7a281e97b1591e841ee8a3
root <root@localhost.localdomain>
2017-08-23 11:35:49 +0800

    update rnn

--- RNNLM/script/force_decode_ptb.sh
+++ RNNLM/script/force_decode_ptb.sh
@@ -9,6 +9,6 @@ export CUDA_VISIBLE_DEVICES=1
 
 python $PY --mode FORCE_DECODE --model_dir $MODEL_DIR \
     --test_path $TEST_PATH \
-    --size 20 --num_layers 2 \
-    --n_epoch 100 --L 15 --n_bucket 3 --force_decode_output force_decode.txt
+    --size 300 --num_layers 2 \
+    --n_epoch 4 --L 100 --n_bucket 10 --force_decode_output force_decode.txt
     
